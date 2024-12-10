2d1fb6ef77fb84452dae6f8afab08ab0ad9dd1a6
alca8781 <123039964+alca8781@users.noreply.github.com>
2024-12-09 23:02:50 -0700

    comments

--- Limit_Break/src/App.jsx
+++ Limit_Break/src/App.jsx
@@ -8,21 +8,24 @@ function App() {
   const [userFear, setUserFear] = useState(''); 
   const [buttonClicked, setButtonClicked] = useState(false);
   
-  
+  // turn on screen 
   function isOnScreen() {
     setSection_ON(true);
     setButtonClicked(true);
   }
 
+  // turn off on screen 
   function isNotOnScreen() {
     setSection_ON(false);
   }
   
+  // display user fear entry from StoredUserFear 
   function handleUserFearInput(fear) {
     setUserFear(fear); 
     setSection_ON(false); 
   }
 
+  // reset app back to start page 
   function handleReset(){ 
     setUserFear(''); 
     setSection_ON(false); 
@@ -39,18 +42,23 @@ function App() {
     <div className="LimitBreak">
       <h1>LIMIT BREAK</h1>
       
+      {/* If the start button hasn't been clicked - show the intro text and start button*/}
       {!buttonClicked && <p>This app is designed to help the user overcome their fears</p>}
-      
-      <div className='UserFear'>
       {!buttonClicked && <button className='start' onClick={isOnScreen}>Start</button>}  
-        {section_ON && <UserFear onFearEntry={handleUserFearInput} />}
-      </div>
 
+      {/* When section_ON is true - show the UserFear component*/}
+        {section_ON && <UserFear onFearEntry={handleUserFearInput} />}
+      
+      {/*show user fear entry */}
       <div className='StoredUserFear'>
         {userFear && (
           <>
            <StoredUserFear User_Fear={userFear} isNotOnScreen={isNotOnScreen} />
+
+           {/*show reset button */}
            <button className='reset' onClick={handleReset}>Reset</button>
+
+           {/*show enter steps button */}
            <button className='create-steps' onClick={handleUserSteps}>Enter Steps</button>
           </>
         )}
