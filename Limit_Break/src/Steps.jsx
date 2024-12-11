16142a3d7eefd77ce6e2e565e151041b3bee2324
Joseph Pelletier <143644756+joey1pelletier@users.noreply.github.com>
2024-12-10 21:39:55 -0700

    Added steps and checkbox

---
+++ Limit_Break/src/Steps.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export default function Steps({ onStepEntry }) {
+  const [STEP, setSTEP] = useState('');
+  const [Message, setMessage]= useState('')
+  const [stepSubmitted, setStepSubmitted] = useState(false);
+
+const handleChange = (event) => {
+    setSTEP(event.target.value);
+  };
+
+  const handleClick = () => {
+    if (STEP.trim() !== '') {
+      onStepEntry(STEP);
+      console.log(STEP); 
+
+      setSTEP(''); 
+      setMessage('to overcome fears, it is important to create smaller goals to work towards overcoming bigger fears')
+      setStepSubmitted(true);
+    }
+  };
+
+  return (
+    <>
+    {!stepSubmitted && (
+        <>
+            <p>What step would you like to add?</p>
+        <div className="Fear">
+          <div className="TheUserFear">
+            <label htmlFor="userfear" className="form-label"></label>
+            <input
+              type="text"
+              className="STEP"
+              name="STEP"
+              placeholder="Enter your step here"
+              onChange={handleChange}
+              value={STEP}
+            />
+            <br />
+            <button type="button" className="EnterFearbtn" onClick={handleClick}>
+              Submit
+            </button>
+          </div>
+        </div>
+        </>
+        
+    )}
+      
+    </>
+  );
+}
